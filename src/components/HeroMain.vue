0e29d0ae706dc72c4b4473f5bd1e902c85e7f155
Julien Minville <minville.julien1@gmail.com>
2024-07-22 20:37:10 -0400

    fix: Update videoSize ref in HeroMain.vue
    Update the videoSize ref in HeroMain.vue to correctly use the window dimensions, resolving the issue with video size.

--- src/components/HeroMain.vue
+++ src/components/HeroMain.vue
@@ -1,106 +1,108 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, watchEffect } from 'vue';
-import YouTube from 'vue3-youtube';
+import { ref, onMounted, onBeforeUnmount, watchEffect, watch } from "vue";
+import YouTube from "vue3-youtube";
 
-const videoSize = ref({ width: window.innerWidth, height: window.innerHeight});
+const videoSize = ref({ width: window.innerWidth, height: window.innerHeight });
 const playerRef = ref<any>(null);
 const isMuted = ref(false);
 
 const onReady = (event: any) => {
-  playerRef.value = event.target; 
+	playerRef.value = event.target;
   playerRef.value.getVideoEmbedCode();
+  
   playerRef.value.playVideo();
-  setInterval(() => { playerRef.value.playVideo(); }, 141 * 1000);
-
-
+	setInterval(() => {
+		playerRef.value.playVideo();
+	}, 141 * 1000);
 };
 
 const toggleMute = () => {
-  if (playerRef.value) {
-    playerRef.value.isMuted()
-    ? (playerRef.value.unMute(), isMuted.value = true)
-    : (playerRef.value.mute(), isMuted.value = false);
-  }
+	if (playerRef.value) {
+		playerRef.value.isMuted()
+			? (playerRef.value.unMute(), (isMuted.value = true))
+			: (playerRef.value.mute(), (isMuted.value = false));
+	}
 };
 
 const playerVars = {
-  controls: 0,
-  modestbranding: 1,
-  showinfo: 0,
-  rel: 0,
+	controls: 0,
+	modestbranding: 1,
+	showinfo: 0,
+	rel: 0,
 };
 
 const updateVideoSize = () => {
-  const aspectRatio = 16 / 9;
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
+	const aspectRatio = 16 / 9;
+	const [windowWidth, windowHeight] = [window.innerWidth, window.innerHeight];
 
-  if (windowWidth / windowHeight > aspectRatio) {
-    videoSize.value = {
-      width: windowHeight * aspectRatio,
-      height: windowHeight,
-    };
-  } else {
-    videoSize.value = {
-      width: windowWidth,
-      height: windowWidth / aspectRatio,
-    };
-  }
+	if (videoSize.value.width !== windowWidth || videoSize.value.height !== windowHeight) {
+		windowWidth / windowHeight > aspectRatio
+			? (videoSize.value = {
+					width: windowHeight * aspectRatio,
+					height: windowHeight,
+			  })
+			: (videoSize.value = {
+					width: windowWidth,
+					height: windowWidth / aspectRatio,
+			  });
+	}
 };
 
-
 onMounted(() => {
-  window.addEventListener('resize', updateVideoSize);
-  updateVideoSize();
+	window.addEventListener("resize", updateVideoSize);
+	updateVideoSize();
 });
 
-// onBeforeUnmount(() => {
-//   window.removeEventListener('resize', updateVideoSize);
-// });
+onBeforeUnmount(() => {
+	window.removeEventListener("resize", updateVideoSize);
+});
 
-// watchEffect(() => {
-//  updateVideoSize();
-// });
+watchEffect(() => {
+  if (videoSize.value.width !== window.innerWidth || videoSize.value.height !== window.innerHeight) {
+    console.log('update video size');
+    updateVideoSize();
+  }
+});
 </script>
 
 <template>
- 
-    <BRow class="flex-grow-1">
-      <BCol class="d-flex align-items-center justify-content-center">
-        <div class="video-wrapper">
-         <div class="screen" :class="{ mute: isMuted, unmute: !isMuted }" @click="toggleMute"></div>
-          <YouTube
-          id="embed-code"
-            src="https://www.youtube.com/embed/ySqSChzNv2U?playlist=ySqSChzNv2U&loop=1"
-            :vars="playerVars"
-            @ready="onReady"
-            :width="videoSize.width"
-            :height="videoSize.height"
-          />
-        </div>
-      </BCol>
-    </BRow>
+	<BRow class="flex-grow-1">
+		<BCol class="d-flex align-items-center justify-content-center">
+			<div class="video-wrapper">
+				<div
+					class="screen"
+					:class="{ mute: isMuted, unmute: !isMuted }"
+					@click="toggleMute"></div>
+				<YouTube
+					id="embed-code"
+					src="https://www.youtube.com/embed/ySqSChzNv2U?playlist=ySqSChzNv2U&loop=1"
+					:vars="playerVars"
+					@ready="onReady"
+					:width="videoSize.width"
+					:height="videoSize.height" />
+			</div>
+		</BCol>
+	</BRow>
 </template>
 
 <style lang="sass" scoped>
-.video-wrapper 
+.video-wrapper
   width: fit-content
   height: fit-content
   position: relative
 
 
-.screen 
+.screen
   position: absolute
   top: 0
   left: 0
   width: 100%
   height: 100%
   z-index: 10
- 
+
 .mute
   cursor: url('src/assets/icons/mute.svg'),auto
 
 .unmute
   cursor: url('src/assets/icons/unmute.svg'),auto
-
-  </style>
+</style>
