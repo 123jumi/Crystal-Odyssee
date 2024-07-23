c9e0a08b3995d69d3de5712ddd811c3c74d57700
Julien Minville <minville.julien1@gmail.com>
2024-07-23 00:01:18 -0400

    feat: Add down class to navigation items in NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,13 +9,18 @@ const { t } = useI18n()
 <template>
   <BNavbar class="navbar p-0" type="dark" variant="dark">
     <BNavbarNav class="w-100 d-flex justify-content-between align-items-center p-0">
-      <BNavItem class="p-2" href="#">{{ t('message.nav.forfaits') }}</BNavItem>
+      <BNavItem class="down p-2" href="#">{{ t('message.nav.forfaits') }}</BNavItem>
       <LogoHolder> </LogoHolder>
-      <BNavItem class="p-2" href="#">{{ t('message.nav.reserver') }}</BNavItem>
+      <BNavItem class="down p-2" href="#">{{ t('message.nav.reserver') }}</BNavItem>
     </BNavbarNav>
   </BNavbar>
 </template>
 
 
 <style lang="sass" scoped>
+.down
+  margin-bottom: 2rem
+@media screen and (max-width: 730px)
+  .down
+    margin-bottom: 1rem
 </style>
